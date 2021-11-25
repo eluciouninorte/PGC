0525c680700e794224412823172a3f971f883ba6
eluciouninorte <elucio@uninorte.edu.co>
2021-11-25 18:10:36 -0500

    Editar la plantilla de bootstrap

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -28,30 +28,29 @@ function Footer() {
         <Nav>
           <NavItem>
             <NavLink href="https://www.creative-tim.com/?ref=bdr-user-archive-footer">
-              Creative Tim
+              Liga de Ping Pong
             </NavLink>
           </NavItem>
           <NavItem>
             <NavLink href="https://www.creative-tim.com/presentation?ref=bdr-user-archive-footer">
-              About Us
+              Acerca de Nosotros
             </NavLink>
           </NavItem>
           <NavItem>
             <NavLink href="https://www.creative-tim.com/blog?ref=bdr-user-archive-footer">
-              Blog
+              Instagram
             </NavLink>
           </NavItem>
         </Nav>
         <div className="copyright">
-          © {new Date().getFullYear()} made with{" "}
-          <i className="tim-icons icon-heart-2" /> by{" "}
+          © {new Date().getFullYear()} Realizado con{" "}
+          <i className="tim-icons icon-heart-2" /> Por {" "}
           <a
             href="https://www.creative-tim.com/?ref=bdr-user-archive-footer"
             target="_blank"
           >
-            Creative Tim
+            PGC
           </a>{" "}
-          for a better web.
         </div>
       </Container>
     </footer>
